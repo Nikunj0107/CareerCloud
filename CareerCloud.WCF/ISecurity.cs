426f6a30be56c31af780ba96fca61e6a61f73b3b
Nikunj Agarwal <nikunj0107@gmail.com>
2019-10-04 20:56:47 -0400

    Added WCF service to do CRUD operations

---
+++ CareerCloud.WCF/ISecurity.cs
@@ -0,0 +1,63 @@
+﻿using CareerCloud.Pocos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CareerCloud.WCF
+{
+    [ServiceContract]
+    public interface ISecurity
+    {
+        [OperationContract]
+        void AddSecurityLogin(SecurityLoginPoco[] items);
+        [OperationContract]
+        void UpdateSecurityLogin(SecurityLoginPoco[] items);
+        [OperationContract]
+        void RemoveSecurityLogin(SecurityLoginPoco[] items);
+        [OperationContract]
+        SecurityLoginPoco GetSingleSecurityLogin(String Id);
+        [OperationContract]
+        List<SecurityLoginPoco> GetAllSecurityLogin();
+
+        [OperationContract]
+        void AddSecurityLoginsLog(SecurityLoginsLogPoco[] items);
+        [OperationContract]
+        void UpdateSecurityLoginsLog(SecurityLoginsLogPoco[] items);
+        [OperationContract]
+        void RemoveSecurityLoginsLog(SecurityLoginsLogPoco[] items);
+        [OperationContract]
+        SecurityLoginsLogPoco GetSingleSecurityLoginsLog(String Id);
+        [OperationContract]
+        List<SecurityLoginsLogPoco> GetAllSecurityLoginsLog();
+
+        [OperationContract]
+        void AddSecurityLoginsRole(SecurityLoginsRolePoco[] items);
+        [OperationContract]
+        void UpdateSecurityLoginsRole(SecurityLoginsRolePoco[] items);
+        [OperationContract]
+        void RemoveSecurityLoginsRole(SecurityLoginsRolePoco[] items);
+        [OperationContract]
+        SecurityLoginsRolePoco GetSingleSecurityLoginsRole(String Id);
+        [OperationContract]
+        List<SecurityLoginsRolePoco> GetAllSecurityLoginsRole();
+
+
+        //RemoveSecurityLoginsRole
+
+        [OperationContract]
+        void AddSecurityRole(SecurityRolePoco[] items);
+        [OperationContract]
+        void UpdateSecurityRole(SecurityRolePoco[] items);
+        [OperationContract]
+        void RemoveSecurityRole(SecurityRolePoco[] items);
+        [OperationContract]
+        SecurityRolePoco GetSingleSecurityRole(String Id);
+        [OperationContract]
+        List<SecurityRolePoco> GetAllSecurityRole();
+    }
+}
+
+
---
+++ CareerCloud.WCF/ISystem.cs
@@ -0,0 +1,35 @@
+﻿using CareerCloud.Pocos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CareerCloud.WCF
+{
+    [ServiceContract]
+    public interface ISystem
+    {
+        [OperationContract]
+        void AddSystemCountryCode(SystemCountryCodePoco[] items);
+        [OperationContract]
+        void UpdateSystemCountryCode(SystemCountryCodePoco[] items);
+        [OperationContract]
+        void RemoveSystemCountryCode(SystemCountryCodePoco[] items);
+        [OperationContract]
+        SystemCountryCodePoco GetSingleSystemCountryCode(String Id);
+        [OperationContract]
+        List<SystemCountryCodePoco> GetAllSystemCountryCode();
+        [OperationContract]
+        void AddSystemLanguageCode(SystemLanguageCodePoco[] items);
+        [OperationContract]
+        void UpdateSystemLanguageCode(SystemLanguageCodePoco[] items);
+        [OperationContract]
+        void RemoveSystemLanguageCode(SystemLanguageCodePoco[] items);
+        [OperationContract]
+        SystemLanguageCodePoco GetSingleSystemLanguageCode(String Id);
+        [OperationContract]
+        List<SystemLanguageCodePoco> GetAllSystemLanguageCode();
+    }
+}
